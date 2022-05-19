883293fb5e23fd396ad275412320c66b25eb57ae
karaianov <dm.kar1996@gmail.com>
2022-05-19 21:32:23 +0300

    Start project

--- src/App.js
+++ src/App.js
@@ -1,23 +1,27 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    <div className="wrapper">
+      
+      <header className='header'>
+        <a href="#" className="logo">Social Network</a>
       </header>
+      
+      <aside className='aside'>
+        <nav className='nav'>
+          <a href="#" className="nav-item">Profile</a>
+          <a href="#" className="nav-item">Messages</a>
+          <a href="#" className="nav-item">News</a>
+          <a href="#" className="nav-item">Music</a>
+          <a href="#" className="nav-item">Settings</a>
+        </nav>
+      </aside>
+
+      <main className='main'>
+        <h1>CONTENT</h1>
+      </main>
+    
     </div>
   );
 }
